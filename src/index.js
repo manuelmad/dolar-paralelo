367bffb77a526e54974519167ecb3d813e086b51
Manuel Araujo <manuelmad1981@gmail.com>
2025-12-06 09:12:30 -0400

    Changing the api, some texts and styles.

--- src/index.js
+++ src/index.js
@@ -1,68 +1,62 @@
-const base_url = "https://pydolarve.org";
+//https://dolarapi.com/docs/venezuela/
 
-const bcv_url = `${base_url}/api/v1/dollar?page=bcv`;
+const base_url = "https://ve.dolarapi.com";
+
+const bcv_url = `${base_url}/v1/dolares/oficial`;
 const bcv_logo = document.getElementById("bcv_logo");
 const bcv_rate_container = document.getElementById("bcv_rate_container");
 
 //const bcv_eur_url = `${base_url}/api/v1/eur?page=bcv`;
 
-const enparalelo_url = `${base_url}/api/v1/dollar?page=enparalelovzla`;
+const enparalelo_url = `${base_url}/v1/dolares/paralelo`;
 const enparalelo_logo = document.getElementById("enparalelo_logo");
 const enparalelo_title = document.getElementById("enparalelo_title");
 const enparalelo_rate_container = document.getElementById("enparalelo_rate_container");
+
+// const bitcoin_url = `${base_url}/v1/dolares/bitcoin`;
+// const bitcoin_logo = document.getElementById("bitcoin_logo");
+// const bitcoin_rate_container = document.getElementById("bitcoin_rate_container");
 
 const average_container = document.getElementById("average_container");
 
 const date = document.getElementById("date");
 
 
-async function getRates(url1) {
+async function getRates(url1, url2) {
     const res1 = await fetch(url1);
     const data1 = await res1.json();
 
-    //const res2 = await fetch(url2);
-    //const data2 = await res2.json();
+    const res2 = await fetch(url2);
+    const data2 = await res2.json();
 
-    console.log(data1);
+    // const res3 = await fetch(url3);
+    // const data3 = await res3.json();
+
 
     let bcv_rate;
     let enparalelo_rate;
+    // let bitcoin_rate;
 
-    if(res1.status !== 200) {
-        console.log("Hubo un error: " + res1.status);
-    } else {
-        bcv_rate = data1.monitors.usd.price;
-        bcv_rate_container.innerHTML = `<span>${bcv_rate.toFixed(1)}</span> Bs./USD`;
-
-        enparalelo_rate = data1.monitors.eur.price;
-        enparalelo_rate_container.innerHTML = `<span>${enparalelo_rate.toFixed(1)}</span> Bs./Euro`
-    }
-
-    //console.log(data2);
-
-    
-/*
-    if(res2.status !== 200) {
-        console.log("Hubo un error: " + res2.status);
-    } else {
-        //const img = document.createElement("img");
-        //img. src = data2.monitors.enparalelovzla.image;
-        //enparalelo_logo.appendChild(img);
-
-        //enparalelo_title.innerHTML = `${data2.monitors.enparalelovzla.title}`
-    }
-*/
     let average_rate;
 
-    if(res1.status == 200) {
+    if(res1.status !== 200 || res2.status !== 200) {
+        console.log("Hubo un error: " + res1.status);
+        average_container.innerHTML = "Ha habido un error al consultar las tasas";
+    } else {
+        bcv_rate = data1.promedio;
+        bcv_rate_container.innerHTML = `<span>${bcv_rate.toFixed(1)}</span> Bs./USD`;
+
+        enparalelo_rate = data2.promedio;
+        enparalelo_rate_container.innerHTML = `<span>${enparalelo_rate.toFixed(1)}</span> Bs./USD`;
+
+        // bitcoin_rate = data3.promedio;
+        // bitcoin_rate_container.innerHTML = `<span>${bitcoin_rate.toFixed(1)}</span> Bs./USD`;
+
         average_rate = (bcv_rate+enparalelo_rate)/2;
         average_container.innerHTML = `<span>${average_rate.toFixed(1)}</span> Bs./USD`;
 
-        date.innerHTML = `${data1.datetime.date}`;
-    } else {
-        average_container.innerHTML = "Ha habido un error al consultar las tasas";
-    }
-    
+        date.innerHTML = `${data1.fechaActualizacion.slice(0,10)}`;
+    }  
 }
 
-getRates(bcv_url);
+getRates(bcv_url, enparalelo_url);
